74c0505942b759526222cb9239e154a0921322f4
koriisabellaa <corryissabella@gmail.com>
2021-11-11 17:14:03 +0700

    Module with ES6 standard

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,3 +1,4 @@
+import DataSource from '../data/data-source.js';
 const main = () => {
     const searchElement = document.querySelector("#searchElement");
     const buttonSearchElement = document.querySelector("#searchButtonElement");
@@ -35,3 +36,5 @@ const main = () => {
 
     buttonSearchElement.addEventListener("click", onButtonSearchClicked);
 };
+
+export default main;
